df0ead5525a942d118ca6fc527fdc91a21b812d3
sujathawgp <sujatha.wgp@gmail.com>
2017-02-27 02:52:06 -0600

    First writeframe commit

---
+++ SodexoWallet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SodexoWallet
+//
+//  Created by Sujatha Nagarajan on 2/27/17.
+//  Copyright © 2017 SwiftBengaluru. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
