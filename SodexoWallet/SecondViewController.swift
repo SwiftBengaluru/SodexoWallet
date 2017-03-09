fafcbf6373a5726ab6fb853536ee2f7268e5d7a3
sujathawgp <sujatha.wgp@gmail.com>
2017-03-09 05:33:43 -0600

    Add basic UI, amount input, pay button. Get amount text input and display in the output label as string.

--- SodexoWallet/SecondViewController.swift
+++ SodexoWallet/SecondViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+	var TotalAmount:UInt32 = 0;
+	@IBOutlet weak var AmountTextField: UITextField!
+	@IBAction func PayButtonClick(_ sender: UIButton, forEvent event: UIEvent) {
+		SetTotalAmount();
+	}
+	
+	@IBOutlet weak var OutputText: UILabel!
+	@IBAction func AmountTextValueChanged(_ sender: Any) {
+		SetTotalAmount()
+	}
+	@IBAction func AmountTextEditingEnded(_ sender: UITextField, forEvent event: UIEvent) {
+			SetTotalAmount();
+	}
+	
+	func SetTotalAmount()
+	{
+		if let t = AmountTextField.text, t != "", let amount = UInt32(t)  {
+			TotalAmount = amount;				
+			print(TotalAmount)
+			OutputText.text = String(TotalAmount) + " X 1"// Debug
+		}
+	
+
+	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
