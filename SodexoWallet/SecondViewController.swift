58f0d1fff5414bacdea1139193c2f80b56c8508f
sujathawgp <sujatha.wgp@gmail.com>
2017-03-12 22:48:43 -0500

    Rough Draft of having debug inventory. Adding to the debug inventory. Using the debug inventory to calculate the coupons to use for the amount using brute force calculation.

--- SodexoWallet/SecondViewController.swift
+++ SodexoWallet/SecondViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+	//Debug placeholder coupon inventory until the actual inventory gets ready.
+	
+	var CouponInventory: [UInt32:UInt32] = [:]
+	
+	
+	// debug ends
+	
 	var TotalAmount:UInt32 = 0;
 	@IBOutlet weak var AmountTextField: UITextField!
 	@IBAction func PayButtonClick(_ sender: UIButton, forEvent event: UIEvent) {
@@ -20,23 +27,63 @@ class SecondViewController: UIViewController {
 	@IBAction func AmountTextValueChanged(_ sender: Any) {
 		SetTotalAmount()
 	}
+	
 	@IBAction func AmountTextEditingEnded(_ sender: UITextField, forEvent event: UIEvent) {
 			SetTotalAmount();
 	}
-	
-	func SetTotalAmount()
-	{
+	 
+	func SetTotalAmount(){
 		if let t = AmountTextField.text, t != "", let amount = UInt32(t)  {
 			TotalAmount = amount;				
-			print(TotalAmount)
-			OutputText.text = String(TotalAmount) + " X 1"// Debug
+			redeem();
 		}
 	
 
 	}
+	
+	func redeem(){
+		//todo prioritize picking 
+				var Total = TotalAmount;
+		var redeemInventory:[UInt32:UInt32] = [:]
+		// Sort and keep the array.
+		while(Total > 0){
+		var SortedInventory = CouponInventory.sorted(by: {(a,b) in 
+								a.key > b.key; });
+
+			for(key, value) in SortedInventory{
+				if(Total < 5) { break; } 
+				print(key , " " , value);
+				var Count = Total / key
+				if(Count > value / 2){
+					Count = Count / 2; // reduce to keep 
+				}
+				Total = Total - ( Count * key )
+				if(redeemInventory[key] != nil){
+					redeemInventory[key] = redeemInventory[key]! + Count;
+				}
+				else{
+					redeemInventory[key] = Count;
+				}
+				CouponInventory[key] = CouponInventory[key]! - Count;
+				print("final : \(Count)")
+				print("total : \(Total)")
+			}
+		}
+		
+		print("total : \(Total)")
+
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		//debug
+		debugAddCoupon(25, CouponCount: 10);
+		debugAddCoupon(5, CouponCount: 50);
+		debugAddCoupon(10, CouponCount: 20);
+		debugAddCoupon(25, CouponCount: 20);
+		debugAddCoupon(50, CouponCount: 40);
+		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -44,6 +91,15 @@ class SecondViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	func debugAddCoupon(_ CouponValue : UInt32, CouponCount : UInt32)
+	{
+		if CouponInventory[CouponValue] != nil{ 
+			CouponInventory[CouponValue] = CouponInventory[CouponValue]! + CouponCount
+		}
+		else{
+			CouponInventory[CouponValue] = CouponCount;
+		}
+	}
 
 }
 
